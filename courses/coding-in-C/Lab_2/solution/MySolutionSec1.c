fd5c329ec1a919ef33c570aa47fbad17e3fd8036
Maikel <reutmai@tbdir.net>
2026-02-19 22:30:09 +0100

    Lab2_Section1_finished

---
+++ courses/coding-in-C/Lab_2/solution/MySolutionSec1.c
@@ -0,0 +1,73 @@
+/* Lab2
+ * File: MySolutionSec1.c
+ * Description: Simple calculator program
+ */
+
+ #include <stdio.h> 
+ 
+ 
+ int main()
+ {  
+    for (int i = 8; i > 0; i--) // Spalten-Schleife von 8 bis 0
+    {   
+        for (int column = 0; column < 8; column++) // Schleife zur Inkrementierung der Buchstaben
+        {
+            char letter = 'A' + column; // Buchstaben beginnen bei A und werden bis H hochgezählt
+            int row = i; // Zahlen von 8 bis 1 in jeder Spalte
+
+
+            printf("%c%d ", letter, row);
+            
+
+
+        }
+
+        printf("\n");
+
+
+       
+    }
+
+     return 0;
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ /*int main()
+ {  
+    // Zeilen von 8 bis 1
+    for(int row = 8; row >= 1; row--)
+        {    // Spalten von A bis H
+             for (int column = 0; column <= 8; column++)
+            {   char letter = 'A' + column;
+                 printf("%c%d ", letter, row);
+                
+            }
+                
+             printf("\n"); // Neue Zeile nach jeder Reihe
+        
+        }
+
+        return 0;
+ } */
